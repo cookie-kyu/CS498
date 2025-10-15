f1400c631c9a3b0bf92c9085b05a5bf646930ab7
cookie-kyu <jingtongwang149@gmail.com>
2025-10-15 11:45:12 -0500

    reverted kinda

--- Homework/hw1/q3.py
+++ Homework/hw1/q3.py
@@ -1,45 +1,65 @@
-###Q3: allreduce###
-###please implement ring_allreduce method, using  pytorch's dist method is not allowed###
+# ###Q3: allreduce###
+# ###please implement ring_allreduce method, using  pytorch's dist method is not allowed###
 
-from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
-import torch
-import torch.distributed as dist
+# from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
+# import torch
+# import torch.distributed as dist
 
-def reduce_scatter(chunks, tmp, world, rank, left, right):
-    #                                                                   #
-    #                                                                   #
-    # your code here: follow slides instruction: do counter-clockwise iteration
-    for i in range(world - 1):
-        send_idx = (rank - i) % world
-        recv_idx = (rank - i - 1) % world
+# def reduce_scatter(chunks, tmp, world, rank, left, right):
+#     #                                                                   #
+#     #                                                                   #
+#     # your code here: follow slides instruction: do counter-clockwise iteration
+#     # for i in range(world - 1):
+#     #     send_idx = (rank - i) % world
+#     #     dest_idx = (rank - i - 1) % world
 
-        send_req = dist.isend(tensor=chunks[send_idx], dst=right)
-        recv_req = dist.irecv(tensor=tmp, src=left)
-        recv_req.wait()
-        send_req.wait()
+#     #     sbuf = chunks[send_idx].clone()
 
-        chunks[recv_idx] += tmp 
-    #                                                                   #
-    #                                                                   #
-    return
+#     #     send_req = dist.isend(sbuf, dst=left)
+#     #     dist.irecv(tmp, src=right)
+#     #     #send_req.wait()
+#     #     chunks[dest_idx] += tmp
+
+#     # return chunks[rank]
+#     for i in range(world - 1):
+#         send_idx = (rank - i) % world
+#         dest_idx = (rank - i - 1) % world
+
+#         sbuf = chunks[send_idx].clone()
+
+#         send_req = dist.isend(sbuf, dst=left)
+#         recv_req = dist.irecv(tmp, src=right)
+#         recv_req.wait()
+#         send_req.wait()
+
+#         chunks[dest_idx] += tmp
+#     return chunks[rank]
+#     #                                                                   #
+#     #                                                                   #
+#     #return
         
-def all_gather(chunks, tmp, current, world, rank, left, right):
-    #                                                                   #
-    #                                                                   #
-    # your code here: follow slides instruction: do counter-clockwise iteration
-    for i in range(world - 1):
-        send_idx = (rank - i - 1) % world
-        recv_idx = (rank - i - 2) % world
+# def all_gather(chunks, tmp, current, world, rank, left, right):
+#     #                                                                   #
+#     #                                                                   #
+#     # your code here: follow slides instruction: do counter-clockwise iteration
+#     chunks[rank] = current.clone()
 
-        send_req = dist.isend(tensor=chunks[send_idx], dst=right)
-        recv_req = dist.irecv(tensor=tmp, src=left)
-        recv_req.wait()
-        send_req.wait()
+#     for i in range(world - 1):
+#         send_idx = (rank + i) % world
+#         recv_idx = (rank + i + 1) % world
 
-        chunks[recv_idx].copy_(tmp)
-    #                                                                   #
-    #                                                                   #
-    return
+#         sbuf = chunks[send_idx]
+
+#         send_req = dist.isend(sbuf, dst=right)
+#         dist.recv(tmp, src=left)
+#         send_req.wait()
+
+#         chunks[recv_idx] = tmp.clone()
+
+#     return torch.cat(chunks, dim=0)
+#     #                                                                   #
+#     #                                                                   #
+#     #return
 
 # def ring_allreduce_(tensor: torch.Tensor, world_size = None, rankid = None):
 #     """In-place ring all-reduce (SUM, optional average) using isend/irecv."""
@@ -56,24 +76,26 @@ def all_gather(chunks, tmp, current, world, rank, left, right):
 #     #                                                                   #
 #     # your code here: we cannot divide flat into 3 pieces evenly as the
 #     # flat lengh may not be able to divided exactly by 3....
-#     padded_size = chunk * world
+    
 #     #
 #     #                                                                   #
 #     #                                                                   #
 #     #So, fill zeros at the end of flat to generate padded_flat
 #     #padded_flat = None # modify this line and fill correct value into padded_flat
     
-#     padded_flat = torch.cat([flat, torch.zeros(padded_size - n, device=flat.device)])
-#     chunks = [padded_flat[i * chunk:(i + 1) * chunk] for i in range(world)]
+#     padded_flat = torch.zeros(chunk * world, dtype=flat.dtype, device=flat.device)
+#     padded_flat[:n] = flat
+#     chunks = [padded_flat[i*chunk:(i+1)*chunk] for i in range(world)]
 
-#     tmp = torch.zeros_like(chunks[0])
 #     #                                                                   #
 #     #                                                                   #
 #     # your code here: call reduce_scatter and all_gather
-#     reduce_scatter(chunks, tmp, world, rank, left, right)
-#     all_gather(chunks, tmp, world, rank, left, right)
-    
-#     flat.copy_(torch.cat(chunks)[:n] / world)
+#     tmp = torch.zeros_like(chunks[0])
+#     reduced_chunk = reduce_scatter(chunks, tmp, world, rank, left, right)
+#     gathered_chunks = all_gather(chunks, tmp, reduced_chunk, world, rank, left, right)
+#     gathered_chunks = gathered_chunks[:n].contiguous()
+
+#     flat.copy_(torch.cat(gathered_chunks)[:n])
 #     #
 #     #                                                                   #
 #     #                                                                   #
@@ -81,10 +103,39 @@ def all_gather(chunks, tmp, current, world, rank, left, right):
 #     # You may adjust the function signature (input structure) of `reduce_scatter` and `all_gather` if needed.
     
 #     # stitch & unpad  
-#     #flat /= world
-#     #tensor.view(-1).copy_(flat[:n])
-#     return tensor
+#     flat /= world
+#     tensor.view(-1).copy_(flat[:n])
+#     return
+from torch._utils import _flatten_dense_tensors, _unflatten_dense_tensors
+import torch
+import torch.distributed as dist
 
+def reduce_scatter(chunks, tmp, world, rank, left, right):
+    """Reduce-scatter phase (counter-clockwise communication)."""
+    for i in range(world - 1):
+        send_idx = (rank - i) % world
+        recv_idx = (rank - i - 1) % world
+
+        send_req = dist.isend(tensor=chunks[send_idx], dst=right)
+        recv_req = dist.irecv(tensor=tmp, src=left)
+        recv_req.wait()
+        send_req.wait()
+
+        chunks[recv_idx] += tmp 
+
+
+def all_gather(chunks, tmp, current,  world, rank, left, right):
+    """All-gather phase (counter-clockwise communication)."""
+    for i in range(world - 1):
+        send_idx = (rank - i - 1) % world
+        recv_idx = (rank - i - 2) % world
+
+        send_req = dist.isend(tensor=chunks[send_idx], dst=right)
+        recv_req = dist.irecv(tensor=tmp, src=left)
+        recv_req.wait()
+        send_req.wait()
+
+        chunks[recv_idx].copy_(tmp)
 
 
 def ring_allreduce_(tensor: torch.Tensor, world_size=None, rankid=None):
